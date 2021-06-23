411a5736856580fe9874f4fd6dc41d88e719d889
Junling <40445944+junling2@users.noreply.github.com>
2021-06-23 15:51:25 -0600

    Add Outline for AQI Activity

--- app/src/main/java/com/junling2/aery/MainActivity.java
+++ app/src/main/java/com/junling2/aery/MainActivity.java
@@ -1,24 +1,42 @@
 package com.junling2.aery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
-    Switch sw_locationsupdates, sw_gps;
+    TextView tv_lat, tv_lon, tv_sensor, tv_updates, tv_address;
+    Switch sw_locations_updates, sw_gps;
+    Button btn_aqi, btn_map;
 
     FusedLocationProviderClient fusedLocationProviderClient;
     LocationRequest locationRequest;
+    LocationCallback locationCallBack;
     boolean updateOn = false;
     public static final int DEFAULT_UPDATE_INTERVAL = 30;
     public static final int FAST_UPDATE_INTERVAL = 5;
+    public static final int PERMISSIONS_FINE_LOCATION = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +46,26 @@ public class MainActivity extends AppCompatActivity {
         // Link XML ID to TextView and Switch Objects
         tv_lat = findViewById(R.id.tv_lat);
         tv_lon = findViewById(R.id.tv_lon);
-        tv_altitude = findViewById(R.id.tv_altitude);
-        tv_accuracy = findViewById(R.id.tv_accuracy);
-        tv_speed = findViewById(R.id.tv_speed);
         tv_sensor = findViewById(R.id.tv_sensor);
         tv_updates = findViewById(R.id.tv_updates);
         tv_address = findViewById(R.id.tv_address);
-        sw_locationsupdates = findViewById(R.id.sw_locationsupdates);
+        sw_locations_updates = findViewById(R.id.sw_locationsupdates);
         sw_gps = findViewById(R.id.sw_gps);
+        btn_aqi = findViewById(R.id.btn_aqi);
+        btn_map = findViewById(R.id.btn_map);
 
         locationRequest = LocationRequest.create();
         locationRequest.setInterval(DEFAULT_UPDATE_INTERVAL * 1000);
         locationRequest.setFastestInterval(FAST_UPDATE_INTERVAL * 1000);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        locationCallBack = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Location location = locationResult.getLastLocation();
+                updateUIValues(location);
+            }
+        };
 
         sw_gps.setOnClickListener(v -> {
             if (sw_gps.isChecked()) {
@@ -51,7 +76,88 @@ public class MainActivity extends AppCompatActivity {
                 tv_sensor.setText("Using Balanced Mode");
             }
         });
+        updateGPS();
 
+        sw_locations_updates.setOnClickListener(v -> {
+            if (sw_locations_updates.isChecked()) {
+                // Location updates on
+                startLocationUpdates();
+            } else {
+                // Location updates off
+                stopLocationUpdates();
+            }
+        });
+    }
 
+    private void stopLocationUpdates() {
+        tv_updates.setText("Off");
+        tv_lat.setText("Not Tracking");
+        tv_lon.setText("Not Tracking");
+        tv_address.setText("Not Tracking");
+        fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
+    }
+
+    private void startLocationUpdates() {
+        tv_updates.setText("On");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack, null);
+        updateGPS();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSIONS_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateGPS();
+                } else {
+                    Toast.makeText(this, "This Application requires GPS permission to work properly", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    private void updateGPS() {
+        // get permission from user to track GPS
+        // get current GPS info from fusedLocationProviderClient
+        // update GPS data to UI
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // got user permission
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, location -> {
+                updateUIValues(location);
+            });
+        } else {
+            // permission not granted
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+            }
+        }
+    }
+
+    private void updateUIValues(Location location) {
+        tv_lat.setText(String.valueOf(location.getLatitude()));
+        tv_lon.setText(String.valueOf(location.getLongitude()));
+        Geocoder geocoder = new Geocoder(MainActivity.this);
+        try {
+            List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            tv_address.setText(addressList.get(0).getAddressLine(0));
+        } catch (IOException e) {
+            tv_address.setText("Error Getting Address");
+        }
     }
 }
---
+++ app/src/main/java/com/junling2/aery/ShowAQIData.java
@@ -0,0 +1,14 @@
+package com.junling2.aery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowAQIData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_aqi_data);
+    }
+}
